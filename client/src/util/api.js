dd1237b2591aef62f5da609ce91971c7f161d2b2
Yannick Ace Schlund <yannick.schlund@allenovery.com>
2026-02-09 20:04:55 +0100

    update api endpoint so you dont have to change it each time you get a new IP

--- client/src/util/api.js
+++ client/src/util/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "http://192.168.178.21:8000"
+    baseURL: "http://" + window.location.hostname + ":8000"
 });
 
 export default api;
